3aff2e26113207c16a584a65e7b2436f48ac6184
Omer Heldenberg <omer.heldenberg@gmail.com>
2024-07-23 01:04:32 +0300

    some db modifications

--- Backend/database.py
+++ Backend/database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+from urllib.parse import quote_plus
 
 class Database:
     _instance = None
@@ -11,9 +11,21 @@ class Database:
 
     def __init__(self):
         if not hasattr(self, 'initialized'):
-            self.client = MongoClient('mongo', 27017)
+            username = quote_plus('omerheldenberg')
+            password = quote_plus('odxlt7xnNigCLjT9')
+            uri = f'mongodb+srv://{username}:{password}@cluster0.iccgxdm.mongodb.net/TaskManagerDB?retryWrites=true&w=majority'
+            self.client = MongoClient(uri)
             self.db = self.client['TaskManagerDB']
-            self.loans_collection = self.db['users']
+            self.users_collection = self.db['users'] 
             self.initialized = True
-    
+
+    def find_one(self, query):
+        return self.users_collection.find_one(query)
+
+    def insert_one(self, data):
+        return self.users_collection.insert_one(data)
+
+    def update_one(self, query, update):
+        return self.users_collection.update_one(query, update)
+
 database = Database()
